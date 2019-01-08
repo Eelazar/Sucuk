6ed06bd21612c64c59369b66d47fb451ce7aa103
Maurice <40207470+Eelazar@users.noreply.github.com>
2019-01-08 18:28:51 +0100

    Moved Wwise variables to separate class + Switched out main orb
    -The Frequency variables from WWise are now processed in a separate class (Wwise Listener)
    - Replaced the orb in Main with the current Wwise version

---
+++ Assets/Scripts/WwiseListener.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WwiseListener : MonoBehaviour {
+
+    public static float[] spectrum = new float[9];
+
+    private int type;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+        //Get the values from Wwise
+        type = 1;
+        AkSoundEngine.GetRTPCValue("Fband1", gameObject, 0, out spectrum[0], ref type);
+        AkSoundEngine.GetRTPCValue("Fband2", gameObject, 0, out spectrum[1], ref type);
+        AkSoundEngine.GetRTPCValue("Fband3", gameObject, 0, out spectrum[2], ref type);
+        AkSoundEngine.GetRTPCValue("Fband4", gameObject, 0, out spectrum[3], ref type);
+        AkSoundEngine.GetRTPCValue("Fband5", gameObject, 0, out spectrum[4], ref type);
+        AkSoundEngine.GetRTPCValue("Fband6", gameObject, 0, out spectrum[5], ref type);
+        AkSoundEngine.GetRTPCValue("Fband7", gameObject, 0, out spectrum[6], ref type);
+        AkSoundEngine.GetRTPCValue("Fband8", gameObject, 0, out spectrum[7], ref type);
+        AkSoundEngine.GetRTPCValue("Mkick", gameObject, 0, out spectrum[8], ref type);
+
+        //Normalizes the value to a value between 0 and 1
+        for (int i = 0; i < spectrum.Length; i++)
+        {
+            spectrum[i] += 48;
+            spectrum[i] /= 48;
+        }
+    }
+}
