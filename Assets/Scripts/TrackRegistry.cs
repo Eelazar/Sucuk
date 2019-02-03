3dac2fbcefe1e0ac749788b2d7fe8779196a3e7a
Raven <raven.rusch@gmail.com>
2019-02-03 21:14:04 +0100

    track registry entries

--- Assets/Scripts/TrackRegistry.cs
+++ Assets/Scripts/TrackRegistry.cs
@@ -8,31 +8,33 @@ public static class TrackRegistry {
     public static List<GameObject> bassOrblings = new List<GameObject>();
     public static List<GameObject> leadOrblings = new List<GameObject>();
 
-    public static float[] destructionTimestamps = new float[] { 15F, 63F, 80F };
+    public static float[] destructionTimestamps = new float[] { 90F, 237F, 328F };
+    public static float[] spawnTimestamps = new float[] { 155F, 268F};
 
     public static int segment;
     
     public static List<string> percussionTracks = new List<string>();
     public static List<string> bassTracks = new List<string>();
     public static List<string> leadTracks = new List<string>();
+    public static List<string> kickTracks = new List<string>();
+    public static List<string> chordTracks = new List<string>();
+
 
     public static List<string> percussionTracksIntro = new List<string> { "intro_perc01", "intro_perc02", "intro_perc03" };
     public static List<string> bassTracksIntro = new List<string> { "intro_bass01", "intro_bass02", "intro_bass03", "intro_bass04", "intro_bass05", "intro_bass06" };
     public static List<string> leadTracksIntro = new List<string> { "intro_lead01", "intro_lead02", "intro_lead03", "intro_lead04", "intro_lead05" };
+    public static List<string> kickTracksIntro = new List<string> { "I_kick01", "I_kick02"};
+    public static List<string> chordTracksIntro = new List<string> { "intro_chords01", "intro_chords02", "intro_chords03", "intro_chords04" };
 
-    public static List<string> percussionTracksBreakdown1 = new List<string> { "intro_perc01", "intro_perc02", "intro_perc03" };
-    public static List<string> bassTracksBreakdown1 = new List<string> { "intro_bass01", "intro_bass02", "intro_bass03", "intro_bass04", "intro_bass05", "intro_bass06" };
-    public static List<string> leadTracksBreakdown1 = new List<string> { "intro_lead01", "intro_lead02", "intro_lead03", "intro_lead04", "intro_lead05" };
-
-    public static List<string> percussionTracksDrop1 = new List<string> { "intro_perc01", "intro_perc02", "intro_perc03" };
-    public static List<string> bassTracksDrop1 = new List<string> { "intro_bass01", "intro_bass02", "intro_bass03", "intro_bass04", "intro_bass05", "intro_bass06" };
-    public static List<string> leadTracksDrop1 = new List<string> { "intro_lead01", "intro_lead02", "intro_lead03", "intro_lead04", "intro_lead05" };
-
-    public static List<string> percussionTracksBreakdown2 = new List<string> { "intro_perc01", "intro_perc02", "intro_perc03" };
-    public static List<string> bassTracksBreakdown2 = new List<string> { "intro_bass01", "intro_bass02", "intro_bass03", "intro_bass04", "intro_bass05", "intro_bass06" };
-    public static List<string> leadTracksBreakdown2 = new List<string> { "intro_lead01", "intro_lead02", "intro_lead03", "intro_lead04", "intro_lead05" };
+    public static List<string> percussionTracksDrop1 = new List<string> { "d1_perc01", "d1_perc02"};
+    public static List<string> bassTracksDrop1 = new List<string> { "d1_bass01", "d1_bass02", "d1_bass03", "d1_bass04"};
+    public static List<string> leadTracksDrop1 = new List<string> { "d1_lead01", "d1_lead02", "d1_lead03", "d1_lead04" };
+    public static List<string> kickTracksDrop = new List<string> { "d1_kick01", "d1_kick02"};
+    public static List<string> chordTracksDrop = new List<string> { "d1_chords01", "d2_chords02"};
 
     public static List<string> percussionTracksDrop2 = new List<string> { "intro_perc01", "intro_perc02", "intro_perc03" };
-    public static List<string> bassTracksDrop2 = new List<string> { "intro_bass01", "intro_bass02", "intro_bass03", "intro_bass04", "intro_bass05", "intro_bass06" };
-    public static List<string> leadTracksDrop2 = new List<string> { "intro_lead01", "intro_lead02", "intro_lead03", "intro_lead04", "intro_lead05" };
+    public static List<string> bassTracksDrop2 = new List<string> { "d2_bass01", "d2_bass02", "d2_bass03", "d2_bass04" };
+    public static List<string> leadTracksDrop2 = new List<string> { "d2_lead01", "d2_lead03", "d2_lead04", "d2_lead05" };
+    public static List<string> kickTracksDrop2 = new List<string> { "d2_kick01", "d2_kick02" };
+    public static List<string> chordTracksDrop2 = new List<string> { "d2_chords01", "d2_chords02", "d2_chords03", "d2_chords04", "d2_chords05", "d2_chords06" };
 }
